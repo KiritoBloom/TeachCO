60cf7c391d13e1a37cf8b40986ac36b7d963c88d
Mohamed Ahmed <knightwing862@gmail.com>
2024-05-23 20:30:40 +0300

    Added mode toggle to landing page for better user experience

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,3 +1,4 @@
+import { ModeToggle } from "@/components/theme-toggle";
 import Image from "next/image";
 import Link from "next/link";
 
@@ -32,6 +33,7 @@ const AuthLayout = ({ children }: { children: React.ReactNode }) => {
               </Link>
             </nav>
             <div className="flex items-center gap-2">
+              <ModeToggle />
               <Link
                 className="inline-flex h-9 items-center justify-center rounded-md bg-gray-900 px-4 text-sm font-medium text-gray-50 shadow transition-colors hover:bg-gray-900/90 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-gray-950 disabled:pointer-events-none disabled:opacity-50 dark:bg-gray-50 dark:text-gray-900 dark:hover:bg-gray-50/90 dark:focus-visible:ring-gray-300"
                 href="#star"
