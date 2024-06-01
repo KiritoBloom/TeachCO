e7258781c983d242daa85d391af744958f19e4c4
Mohamed Ahmed <knightwing862@gmail.com>
2024-06-01 20:17:00 +0300

    Bug fix in deployement

--- components/post-container.tsx
+++ components/post-container.tsx
@@ -158,7 +158,7 @@ const PostContainer = ({
                     />
                   </AlertDialogTitle>
                   <AlertDialogDescription>
-                    This will permanently change the edit's content
+                    This will permanently change the edit&upos;s content
                   </AlertDialogDescription>
                 </AlertDialogHeader>
                 <Input
